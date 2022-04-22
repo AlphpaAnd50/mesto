71382f0cb56e60198a2f04e080c56a59c18f5b7b
AlphpaAnd50 <98957294+AlphpaAnd50@users.noreply.github.com>
2022-04-22 14:37:58 +0300

    завершение проекта

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-let nickname = document.querySelector('.profile__nickname');
-let profession = document.querySelector('.profile__profession');
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button')
-let popupNickname = document.querySelector('.popup__nickname');
-let popupProfession = document.querySelector('.popup__profession');
 let saveButton = document.querySelector('.popup__save-button');
 
 function popupActive() {
+  let popup = document.querySelector('.popup');
+
   popup.style.display = 'flex';
 };
 
 function popupInactive() {
+  let popup = document.querySelector('.popup');
+
   popup.style.display = 'none';
 };
 
 function editProfile() {
+  let popup = document.querySelector('.popup');
+  let nickname = document.querySelector('.profile__nickname');
+  let profession = document.querySelector('.profile__profession');
+  let popupNickname = document.querySelector('.popup__nickname');
+  let popupProfession = document.querySelector('.popup__profession');
+
   nickname.textContent = popupNickname.value;
   profession.textContent = popupProfession.value;
   popup.style.display = 'none';
+
+  popupNickname.value = nickname.textContent;
+  popupProfession.value = profession.textContent;
 };
-
-
-popupNickname.value = nickname.textContent;
-popupProfession.value = profession.textContent;
 
 editButton.addEventListener('click', popupActive);
 closeButton.addEventListener('click', popupInactive);
