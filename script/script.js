0394efe83676851b9c863a4a92de4caa32dd7571
AlphpaAnd50 <98957294+AlphpaAnd50@users.noreply.github.com>
2022-04-25 11:05:49 +0300

    работа над ошипками 2

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@ let profession = document.querySelector('.profile__profession');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
-let popupNickname = document.querySelector('.popup__input_nickname');
-let popupProfession = document.querySelector('.popup__input_profession');
-let saveButton = document.querySelector('.popup__form');
+let popupNickname = document.querySelector('.popup__input_type_nickname');
+let popupProfession = document.querySelector('.popup__input_type_profession');
+let profileForm = document.querySelector('.popup__form');
 
 function popupActive() {
 
@@ -26,9 +26,9 @@ function editProfile(evt) {
   nickname.textContent = popupNickname.value;
   profession.textContent = popupProfession.value;
 
-  popup.classList.remove('popup_opened');
+  popupInactive();
 }
 
 editButton.addEventListener('click', popupActive);
 closeButton.addEventListener('click', popupInactive);
-saveButton.addEventListener('submit', editProfile);
+profileForm.addEventListener('submit', editProfile);
